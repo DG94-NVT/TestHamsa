99f33286ac0cf2b394e1204cd00924f06e42714c
DG94-NVT <tonnguyencn94@gmail.com>
2022-05-24 08:55:37 +0700

    cmit_release

--- script/main.js
+++ script/main.js
@@ -1,6 +1,5 @@
 const header = document.querySelector(".header");
 const header_hight = document.querySelector(".header").clientHeight;
-console.log(header_hight);
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > header_hight) {
